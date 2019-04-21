7804a7b9388db3935afa554ed5de917e3c8952b2
YMK <ymk@inbox.ru>
2019-04-21 22:49:23 +0300

    task 2

--- index.php
+++ index.php
@@ -64,6 +64,47 @@ if( $a >= 0 && $b >= 0 ){
     echo 'a+b='.($a+$b).'<br>';
 }
 
+/*
+2. Присвоить переменной $а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от $a до 15.
+*/
+$a=10;
+switch($a) {
+    case 0:
+        echo '0<br>';
+    case 1:
+        echo '1<br>';
+    case 2:
+        echo '2<br>';
+    case 3:
+        echo '3<br>';
+    case 4:
+        echo '4<br>';
+    case 5:
+        echo '5<br>';
+    case 6:
+        echo '6<br>';
+    case 7:
+        echo '7<br>';
+    case 8:
+        echo '8<br>';
+    case 9:
+        echo '9<br>';        
+    case 10:
+        echo '10<br>';        
+    case 11:
+        echo '11<br>';        
+    case 12:
+        echo '12<br>';        
+    case 13:
+        echo '13<br>';        
+    case 14:
+        echo '14<br>';        
+    case 15:
+        echo '15<br>';        
+    default:
+        break;
+}
+
         
 //////////////////////////////////////////////////////////////////////////////////
 //

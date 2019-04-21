eb0e6d4f267f8ce8c563e342c259917256132ae1
YMK <ymk@inbox.ru>
2019-04-21 22:52:18 +0300

    task 3

--- index.php
+++ index.php
@@ -105,6 +105,29 @@ switch($a) {
         break;
 }
 
+/*
+3. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+*/
+
+function add($a,$b) {
+    return $a+$b;
+}
+function sub($a,$b) {
+    return $a-$b;
+}
+function div($a,$b) {
+    return $a/$b;
+}
+function mul($a,$b) {
+    return $a*$b;
+}
+$a=2;
+$b=3;
+echo add($a,$b)."<br>";
+echo sub($a,$b)."<br>";
+echo div($a,$b)."<br>";
+echo mul($a,$b)."<br>";
+
         
 //////////////////////////////////////////////////////////////////////////////////
 //
